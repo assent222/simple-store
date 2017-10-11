9ed7b78adaf037aa0ed867cd33cbdc8c8a49417b
Kyrylo Pysarenko <Kyrylo_Pysarenko@epam.com>
2017-10-11 19:02:46 +0300

    add slf4j+logback

--- src/main/java/pkk/simplestore/App.java
+++ src/main/java/pkk/simplestore/App.java
@@ -1,11 +1,17 @@
 package pkk.simplestore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  */
 public class App {
 
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
         System.out.println("hello");
+        logger.info("hello logger");
     }
 }
--- src/test/java/pkk/simplestore/AppTest.java
+++ src/test/java/pkk/simplestore/AppTest.java
@@ -2,14 +2,19 @@ package pkk.simplestore;
 
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
+    private static Logger logger = LoggerFactory.getLogger(AppTest.class);
+
     @Test
     public void hello() throws Exception {
         System.out.println("hello");
+        logger.info("hello logger");
     }
 }
