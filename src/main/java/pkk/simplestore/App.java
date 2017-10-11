5dea79f34bb31297074d65d5d07828378bdadd3c
Kyrylo Pysarenko <Kyrylo_Pysarenko@epam.com>
2017-10-11 18:16:06 +0300

    Initial commit

---
+++ src/main/java/pkk/simplestore/App.java
@@ -0,0 +1,11 @@
+package pkk.simplestore;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/pkk/simplestore/AppTest.java
@@ -0,0 +1,15 @@
+package pkk.simplestore;
+
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void hello() throws Exception {
+        System.out.println("hello");
+    }
+}
